e8fa888d603e2d7983c7919e59aca8642e451863
David Lima <davidprofissional@gmail.com>
2024-01-21 18:43:40 -0300

    Spring-Security e JWT token

---
+++ src/main/java/com/example/auth/dtos/ProductResponseDTO.java
@@ -0,0 +1,9 @@
+package com.example.auth.dtos;
+
+import com.example.auth.domain.product.Product;
+
+public record ProductResponseDTO(String id, String name, Integer price) {
+    public ProductResponseDTO(Product product){
+        this(product.getId(), product.getName(), product.getPrice());
+    }
+}
